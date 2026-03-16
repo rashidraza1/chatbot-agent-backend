ce1cb6d1e839491723e1939b2abf6e5d8d30853f
Rashid Raza <rashidansari.gsb11@gmail.com>
2026-03-16 16:27:47 +0530

    initail setup

--- config/database.js
+++ config/database.js
@@ -2,6 +2,8 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const mysql2 = require('mysql2');
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -10,6 +12,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
+    dialectModule: mysql2, // Critical fix for Vercel/serverless environments
     logging: false,
   }
 );
